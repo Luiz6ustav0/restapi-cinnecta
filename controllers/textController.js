b8089974bb8597a3aa05ac39b3c94b8c1da9ece5
Luiz Almeida <luizorst@hotmail.com>
2021-02-21 00:55:52 -0300

    [ADD] controllers for textApi

---
+++ controllers/textController.js
@@ -0,0 +1,59 @@
+const Text = require("../models/texts");
+
+const getVocab = async (req, res) => {
+  try {
+    const textInfo = await Text.findOne({});
+    if (textInfo.vocab && textInfo.vocab.length > 0) {
+      res.status(200).json(textInfo.vocab);
+    } else {
+      res.status(404).json(err);
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
+
+const getSingleWordVecs = async (req, res) => {
+  try {
+    const textInfo = await Text.find({});
+    if (textInfo && textInfo[0].singleWordFrequencyVecs.length > 0) {
+      res
+        .status(200)
+        .json({ vectorsPerDocument: textInfo[0].singleWordFrequencyVecs });
+    } else {
+      res.status(404).json(err);
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
+
+const postNTexts = (req, res) => {
+  const body = req.body;
+  const tVec = new textToVec();
+
+  let cleanedTexts = Object.keys(body).map(function (key) {
+    const text = {
+      key: key,
+      body: body[key],
+    };
+    return tVec.cleanText(text.body);
+  });
+  cleanedTexts.map((arr) => tVec.createVocab(arr));
+  let frequencyArrs = cleanedTexts.map((arr) => tVec.createFrequencyVec(arr));
+
+  let newItem = Text({
+    vocab: tVec.vocabulary,
+    singleWordFrequencyVecs: frequencyArrs,
+    twoWordsFrequencyVecs: frequencyArrs,
+  });
+
+  newItem
+    .save()
+    .then((result) => {
+      res.status(201).send();
+    })
+    .catch((err) => console.log(err));
+};
+
+module.exports = { getVocab, getSingleWordVecs, postNTexts };
--- routes/textApi.js
+++ routes/textApi.js
@@ -1,64 +1,14 @@
 const express = require("express");
 const router = express.Router();
 const textToVec = require("../utils/textToVec");
+const textController = require("../controllers/textController");
 
 const Text = require("../models/texts");
 
-router.get("/vocab", async (req, res) => {
-  try {
-    const textInfo = await Text.findOne({});
-    if (textInfo.vocab && textInfo.vocab.length > 0) {
-      res.status(200).json(textInfo.vocab);
-    } else {
-      res.status(404).json(err);
-    }
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
+router.get("/vocab", textController.getVocab);
 
-router.get("/singleWordVecs", async (req, res) => {
-  try {
-    const textInfo = await Text.find({});
-    console.log(textInfo[0]);
-    if (textInfo && textInfo[0].singleWordFrequencyVecs.length > 0) {
-      res
-        .status(200)
-        .json({ vectorsPerDocument: textInfo[0].singleWordFrequencyVecs });
-    } else {
-      res.status(404).json(err);
-    }
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
+router.get("/singleWordVecs", textController.getSingleWordVecs);
 
-router.post("/", (req, res) => {
-  const body = req.body;
-  const tVec = new textToVec();
-
-  let cleanedTexts = Object.keys(body).map(function (key) {
-    const text = {
-      key: key,
-      body: body[key],
-    };
-    return tVec.cleanText(text.body);
-  });
-  cleanedTexts.map((arr) => tVec.createVocab(arr));
-  let frequencyArrs = cleanedTexts.map((arr) => tVec.createFrequencyVec(arr));
-
-  let newItem = Text({
-    vocab: tVec.vocabulary,
-    singleWordFrequencyVecs: frequencyArrs,
-    twoWordsFrequencyVecs: frequencyArrs,
-  });
-
-  newItem
-    .save()
-    .then((result) => {
-      res.status(201).send();
-    })
-    .catch((err) => console.log(err));
-});
+router.post("/", textController.postNTexts);
 
 module.exports = router;
