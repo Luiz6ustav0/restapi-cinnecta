1fb8d8d42994078b0a97de7350b3224857f7ba3e
Luiz Almeida <luizorst@hotmail.com>
2021-02-20 23:59:39 -0300

    [FIX] remove unnecessary function

--- utils/textToVec.js
+++ utils/textToVec.js
@@ -23,24 +23,11 @@ class textToVec {
 
     for (let i = 0; i < this.vocabulary.length; ++i) {
       if (vec.includes(this.vocabulary[i])) {
-        freqVec1[i] = vec.filter(w => w == this.vocabulary[i]).length;
+        freqVec1[i] = vec.filter((w) => w == this.vocabulary[i]).length;
       }
     }
 
     return freqVec1;
-  }
-
-  calculateAll(text1, text2) {
-    let arr1 = this.cleanText(text1);
-    let arr2 = this.cleanText(text2);
-    let vocab = [...this.createVocab(arr1, arr2)]; // Converting set back to arr
-    let freqVecs = this.creteFrequencyVecs(vocab, arr1, arr2);
-
-    return {
-      vocabulary: vocab,
-      vec1: freqVecs.freqVec1,
-      vec2: freqVecs.freqVec2,
-    };
   }
 }
 
